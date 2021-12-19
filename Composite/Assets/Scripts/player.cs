9068197ad18252411532a1aeb13a18e4b9502c32
Devi Acharya <dacharya64@gmail.com>
2021-12-19 14:00:37 -0800

    working on enemy collision detection

--- Composite/Assets/Scripts/player.cs
+++ Composite/Assets/Scripts/player.cs
@@ -6,7 +6,7 @@ public class player : MonoBehaviour
 {
     public float speed = 10f;
     public float rotateSpeed = 100f;
-
+    public float hitRadius = 2f;
 
     void Start()
     {
@@ -16,18 +16,55 @@ public class player : MonoBehaviour
     private void Update()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
+        // movement and rotation 
         if (Input.GetKey(KeyCode.A))
-            rb.AddForce(-transform.right * speed);
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.D))
-            rb.AddForce(transform.right * speed);
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.W))
             rb.AddForce(transform.forward * speed);
         if (Input.GetKey(KeyCode.S))
             rb.AddForce(-transform.forward * speed);
         if (Input.GetKey(KeyCode.E))
-            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+            rb.AddForce(transform.right * speed);
         if (Input.GetKey(KeyCode.Q))
-            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+            rb.AddForce(-transform.right * speed);
 
+        // attacking enemies
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("mouse down");
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, hitRadius);
+            Debug.Log(transform.position);
+            foreach (var hitCollider in hitColliders)
+            {
+                Debug.Log(hitCollider);
+                if (hitCollider.gameObject.tag == "Enemy") {
+                    /*Debug.Log("found a hit");*/
+                    /*float dot = Vector3.Dot(transform.forward, (hitCollider.gameObject.transform.position - transform.position).normalized);
+                    if (dot > 0.7f)
+                    {*/
+                    Debug.Log(hitCollider.gameObject.GetComponent<Renderer>().isVisible);
+                        if (hitCollider.gameObject.GetComponent<enemy>() != null && hitCollider.gameObject.GetComponent<MeshRenderer>().isVisible)
+                        {
+                            hitCollider.gameObject.GetComponent<enemy>().health = hitCollider.gameObject.GetComponent<enemy>().health - 2;
+                            Debug.Log(hitCollider.gameObject.GetComponent<enemy>().health);
+                            if (hitCollider.gameObject.GetComponent<enemy>().health <= 0) {
+                                Destroy(hitCollider.gameObject);
+                            }
+                        }
+                    /*}
+                    else
+                    {
+                        Debug.Log("not facing");
+                    }*/
+                }
+            }
+
+            
+
+
+        }
+        if (Input.GetMouseButtonDown(1))
+            Debug.Log("Pressed secondary button.");
     }
 }
